9621bd471ce88a8a4fc907707d17e25654332706
Koutarou Murabayashi <s20C1116HC@s.chibakoudai.jp>
2021-12-15 10:03:47 +0000

    Add a explanation

--- myled.c
+++ myled.c
@@ -1,3 +1,9 @@
+//SPDX-LICENSE-IDENTIFIER:GPL-3.0
+/*
+*Copyright(c)2021 Ryuichi Ueda. Yuma Ito. All rights reserved.
+*/
+
+
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
